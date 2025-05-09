8a8feab054cad9773769cbfae57d7f8148dfe0ca
shritikav <shritikaverma24@gmail>
2025-05-09 22:39:16 +0530

    changes home

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,37 +1,85 @@
+const express = require("express");
+const Job = require("../models/Job");
+const Application = require("../models/Application");
 
-const express = require('express');
 const router = express.Router();
-const User = require('../models/User');
-
 
 router.get('/signup', (req, res) => {
   res.render('signup');
 });
 
-
-router.post('/signup', async (req, res) => {
-  const { name, email, password } = req.body;
-  try {
-    await User.create({ name, email, password });
-    res.redirect('/login');
-  } catch (error) {
-    res.send('Signup failed. Email might already be used.');
-  }
-});
-
-
 router.get('/login', (req, res) => {
   res.render('login');
 });
 
+const User = require('../models/User');
+
+router.post('/signup', async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    // Basic validation can be added here
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).send('User with this email already exists');
+    }
+
+    const newUser = new User({ name, email, password });
+    await newUser.save();
+
+    // Optionally, set session user here
+    req.session.user = newUser;
+
+    res.redirect('/');
+  } catch (error) {
+    console.error('Error during signup:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
 router.post('/login', async (req, res) => {
-  const { email, password } = req.body;
-  const user = await User.findOne({ email, password });
-  if (user) {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email, password });
+    if (!user) {
+      return res.status(401).send('Invalid email or password');
+    }
+
+    req.session.user = user;
     res.redirect('/');
-  } else {
-    res.send('Invalid credentials.');
+  } catch (error) {
+    console.error('Error during login:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+// Job Listings Page
+router.get("/", async (req, res) => {
+  const jobs = await Job.find();
+  res.render("index", { jobs });
+});
+
+// Apply Route
+router.post("/apply/:jobId", async (req, res) => {
+  if (!req.session.user) {
+    return res.status(401).json({ redirect: "/signup" });
+  }
+
+  const { jobId } = req.params;
+  const applicantEmail = req.session.user.email;
+
+  try {
+    // Save the application to MongoDB
+    await Application.create({
+      jobId,
+      applicantEmail,
+    });
+
+    return res.status(200).json({ message: "Application submitted" });
+  } catch (err) {
+    console.error("Error saving application:", err);
+    return res.status(500).json({ message: "Failed to submit application" });
   }
 });
 
--- routes/job.js
+++ routes/job.js
@@ -1,48 +1,37 @@
-// File: routes/jobRouter.js
 const express = require("express");
 const Job = require("../models/Job");
+const Application = require("../models/Application");
 
 const router = express.Router();
-
 
 router.get("/", async (req, res) => {
   const jobs = await Job.find();
   res.render("index", { jobs });
 });
 
-
-router.get("/jobs/new", (req, res) => {
-  res.render("newJob");
+router.get("/jobs", async (req, res) => {
+  const jobs = await Job.find();
+  res.render("jobListings", { jobs });
 });
 
-
-router.post("/jobs", async (req, res) => {
-  await Job.create(req.body);
-  res.redirect("/");
+router.get('/jobs/new', (req, res) => {
+  res.render('newJob');
 });
 
+router.post("/apply/:jobId", async (req, res) => {
+  if (!req.session.user) {
+    return res.status(401).json({ redirect: '/signup' });
+  }
 
-router.get("/jobs/:id/edit", async (req, res) => {
-  const job = await Job.findById(req.params.id);
-  res.render("editJob", { job });
+  const { jobId } = req.params;
+  const applicantEmail = req.session.user.email;
+
+  try {
+    await Application.create({ jobId, applicantEmail });
+    res.status(200).send("Application submitted");
+  } catch (error) {
+    res.status(500).send("Failed to submit application");
+  }
 });
-
-
-router.post("/jobs/:id", async (req, res) => {
-  await Job.findByIdAndUpdate(req.params.id, req.body);
-  res.redirect("/");
-});
-
-router.post("/jobs/:id/delete", async (req, res) => {
-  await Job.findByIdAndDelete(req.params.id);
-  res.redirect("/");
-});
-
-router.get("/search", async (req, res) => {
-  const keyword = req.query.keyword;
-  const jobs = await Job.find({ title: new RegExp(keyword, "i") });
-  res.render("index", { jobs });
-});
-
 
 module.exports = router;
